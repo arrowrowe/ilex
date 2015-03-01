7797b4c13fbe151924ca0717114b6ad4de37acbf
Arrow Rowe <arrowrowe@gmail.com>
2015-03-01 12:55:55 +0800

    Add better validation in BaseModel instead of BaseController

--- src/base/model/Base.php
+++ src/base/model/Base.php
@@ -4,6 +4,7 @@
 /**
  * Class BaseModel
  * @property \MongoDB $db database
+ * @property \MongoCollection $collection
  */
 class BaseModel
 {
@@ -26,9 +27,63 @@ class BaseModel
         return is_null($this->$name) ? ($this->$name = Ilex\Loader::model($path)) : $this->$name;
     }
 
-    public function seliectCollection($name)
+    public function selectCollection($name)
     {
         return $this->collection = $this->load_db()->selectCollection($name);
     }
 
+    public function validBatch($values, $rulePackages)
+    {
+        $length = min(count($values), count($rulePackages));
+        $errors = array();
+        for ($i = 0; $i < $length; $i++) {
+            $results = $this->validRules($values[$i], $rulePackages[$i]);
+            if ($results !== TRUE) {
+                $errors[isset($rulePackages[$i]['name']) ? $rulePackages[$i]['name'] : $i] = $results;
+            }
+        }
+        return count($errors) ? $errors : TRUE;
+    }
+
+    public function validRules($value, $rulePackage)
+    {
+        $errors = array();
+        foreach ($rulePackage as $ruleName => $rule) {
+            if ($ruleName === 'name') {
+                continue;
+            }
+            $result = $this->validRule($value, $ruleName, $rule, $rule['message']);
+            if ($result !== TRUE) {
+                $errors[] = $result;
+            }
+        }
+        return count($errors) ? $errors : TRUE;
+    }
+
+    public function validRule($value, $ruleName, $rule, $message = FALSE)
+    {
+        return ValidateLib::$ruleName($value, $rule) ? TRUE : $message;
+    }
+
+}
+
+
+class ValidateLib
+{
+    public static function re($value, $rule) { return preg_match($rule['pattern'], $value) === 1; }
+
+    public static function    int_gt($value, $rule) { return   intval($value) >  $rule['value']; }
+    public static function    int_lt($value, $rule) { return   intval($value) <  $rule['value']; }
+    public static function    int_ge($value, $rule) { return   intval($value) >= $rule['value']; }
+    public static function    int_le($value, $rule) { return   intval($value) <= $rule['value']; }
+
+    public static function  float_gt($value, $rule) { return floatval($value) >  $rule['value']; }
+    public static function  float_lt($value, $rule) { return floatval($value) <  $rule['value']; }
+    public static function  float_ge($value, $rule) { return floatval($value) >= $rule['value']; }
+    public static function  float_le($value, $rule) { return floatval($value) <= $rule['value']; }
+
+    public static function length_gt($value, $rule) { return   strlen($value) >  $rule['value']; }
+    public static function length_lt($value, $rule) { return   strlen($value) <  $rule['value']; }
+    public static function length_ge($value, $rule) { return   strlen($value) >= $rule['value']; }
+    public static function length_le($value, $rule) { return   strlen($value) <= $rule['value']; }
 }
