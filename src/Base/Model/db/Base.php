6d2c473f5ca6be0f45db23676a94e8c9dac9d504
Arrow Rowe <arrowrowe@gmail.com>
2015-05-17 23:59:20 +0800

    Add kit functions for database mongo

--- src/Base/Model/db/Base.php
+++ src/Base/Model/db/Base.php
@@ -22,4 +22,33 @@ class Base extends \Ilex\Base\Model\Base
     {
         $this->collection = Loader::db()->selectCollection($this->collectionName);
     }
+
+    protected function getId($id)
+    {
+        if (is_string($id)) {
+            try {
+                return new \MongoId($id);
+            } catch (\Exception $e) {
+                return $id;
+            }
+        } else {
+            return $id;
+        }
+    }
+
+    protected function setRetractId($object)
+    {
+        if (isset($object['_id'])) {
+            $object['_id'] = $this->getId($object['_id']);
+        }
+        return $object;
+    }
+
+    public function find($criterion = [], $projection = [], $toArray = TRUE)
+    {
+        $criterion = $this->setRetractId($criterion);
+        $cursor = $this->collection->find($criterion, $projection);
+        return $toArray ? array_values(iterator_to_array($cursor)) : $cursor;
+    }
+
 }
